a923b823a7b53304eb38a1b0377e5c9315403fcf
majidalaeinia <alaeinia.maijd@gmail.com>
2020-11-15 14:07:59 +0330

    Remove 'jam' helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,19 +1,5 @@
 <?php
 
-if (! function_exists('jam')) {
-    /**
-     * Add an element to an array using "dot" notation if it doesn't exist.
-     *
-     * @param $number1
-     * @param $number2
-     * @return array
-     */
-    function jam($number1, $number2)
-    {
-        return $number1 - $number2;
-    }
-}
-
 if (! function_exists('resolve_pagination_links')) {
     /**
      * Resolve pagination links.
